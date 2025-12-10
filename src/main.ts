72e6a9102a68803b14c8bb06d5add494e5bee0c1
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2025-12-10 13:22:45 +0100

    Install some package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable global validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Todo API')
+    .setDescription('API for managing todos')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
