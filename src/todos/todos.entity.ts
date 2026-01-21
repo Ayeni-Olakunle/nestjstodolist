68efe7c4a6c84200c85254d4a40b33e759f653dc
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-21 14:49:39 +0100

    Update 5

---
+++ src/todos/todos.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Todos {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+    
+    @Column({ default: 'false' })
+    isCompleted: string;
+}
