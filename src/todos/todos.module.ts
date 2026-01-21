209cf7256edb1621b6fef4d83bd559d8887171fa
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-21 14:56:46 +0100

    Update 6

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TodosController } from './todos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodosService } from './todos.service';
+import { Todos } from './todos.entity';
 
 @Module({
   controllers: [TodosController],
-  providers: [TodosService]
+  providers: [TodosService],
+  imports: [TypeOrmModule.forFeature([Todos])],
 })
+
 export class TodosModule {}
+ 
