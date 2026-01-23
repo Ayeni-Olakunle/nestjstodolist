b658c64ff5ee47e32d8fc11153512857a1e53f2b
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-23 12:09:38 +0100

    Update 21

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -10,4 +10,10 @@ export class Users {
 
     @Column()
     password: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+    updatedAt: Date;
 }
