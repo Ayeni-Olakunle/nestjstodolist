c97d9424092f9f631430bc59e2ff54e2836e63d7
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-26 12:09:14 +0100

    Update 22

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, HttpCode, HttpStatus,  } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { BaseUserDto } from 'src/users/base-user.dto';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+}
