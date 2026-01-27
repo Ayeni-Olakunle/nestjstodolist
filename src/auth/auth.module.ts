93c7db743a4319f208e05dfba5b962900fcfe6ea
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-27 16:01:03 +0100

    Update 37

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-// import { AuthService } from './auth.service';
+import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+// import { AuthController } from './auth.controller';
+// import { AuthService } from './auth.service';
 
 @Module({
-  controllers: [AuthController],
-  // providers: [AuthService],
-  imports: [UsersModule]
+  imports: [UsersModule, JwtModule.register({
+    secret: process.env.JWT_SECRET || 'defaultSecretKey',
+    signOptions: { expiresIn: '60m' },
+  })],
+  providers: [AuthService],
+  exports: [AuthService],
 })
+
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
-// const hashPassword = await bcrypt.hash('myPassword', 10);
-// await bcrypt.compare('myPassword', hashPassword);
 
-// @Injectable()
-// export class AuthService {
-//     constructor (
-//         private usersService: UsersService,
-//         private jwtService: JwtService,
-//     ) {}
-// }
+@Injectable()
+export class AuthService {
+    constructor (
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {}
+}
