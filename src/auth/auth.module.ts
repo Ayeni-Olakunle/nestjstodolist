66ef27839154b6d280b6b333e30362f672ce5282
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-27 17:07:07 +0100

    Update 42

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,19 +4,19 @@ import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
-// import { AuthController } from './auth.controller';
-// import { AuthService } from './auth.service';
 
 @Module({
   imports: [UsersModule, JwtModule.register({
     secret: process.env.JWT_SECRET || 'defaultSecretKey',
     signOptions: { expiresIn: '60m' },
   })],
+
   providers: [
     AuthService,
     LocalStrategy,
     JwtStrategy
   ],
+
   exports: [AuthService],
 })
 
