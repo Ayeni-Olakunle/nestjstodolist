d1e9ae5e52cf451e70fb40b10bf63ff17df8a5ef
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-27 15:40:29 +0100

    Update 35

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+    async createUser(email: string, password: string): Promise<User> {
+        const user = this.usersRepository.create({ email, password });
+        return this.usersRepository.save(user);
+    }
+
+    async findByEmail(email: string): Promise<User | undefined> {
+        return this.usersRepository.findOne({ where: { email } });
+    }
+
+}
