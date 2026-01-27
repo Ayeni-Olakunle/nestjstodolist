4fd1dbe3d831a95984436f975476d140964277cb
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-27 16:55:31 +0100

    Update 40

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { AuthService } from "../auth.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private authService: AuthService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET || 'defaultSecretKey',
+        });
+    }
+    async validate(payload: any) {
+        const user = await this.authService.validateUser(payload.email, null);
+        if (!user) {
+            throw new Error('Unauthorized');
+        }
+        return user;
+    }
+}
