2b6fb9f99c32cc845d6e2cc7927e9ba1b75f6a43
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-28 15:44:34 +0100

    Update 56

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
