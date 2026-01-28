4af7c6d0cecc5bc539a1b2d30c5920ef98d4ebdd
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-28 13:50:26 +0100

    Update 54

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,30 @@
 import { Controller } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Body, Get, Post, Param, Put, Delete } from '@nestjs/common';
+import { User } from './users.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
     @Post()
-    async create(@Body() userData: { email: string; [key: string]: any }) {
-        const { email, ...data } = userData;
+    async create(@Body() user: Partial<User>) {
+        const { email, ...data } = user;
         return this.usersService.create(email, data);
     }
 
-    @Get(':email')
-    async findByEmail(@Param('email') email: string) {
-        return this.usersService.findByEmail(email);
+    @Get(':id')
+    async findById(@Param('id') id: string) {
+        return this.usersService.findById(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() userData: { email: string; [key: string]: any }) {
+        const { email, ...data } = userData;
+        return this.usersService.update(id, email, data);
+    }
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        await this.usersService.delete(id);
     }
 }
