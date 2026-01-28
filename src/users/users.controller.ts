0b7689686b46184947683cbbd254659211827387
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-01-28 12:34:37 +0100

    Update 52

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Body, Get, Post, Param, Put, Delete } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    async create(@Body() userData: { email: string; [key: string]: any }) {
+        const { email, ...data } = userData;
+        return this.usersService.create(email, data);
+    }
+}
