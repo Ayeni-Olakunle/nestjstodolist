dfc0bb24fc9b11307347b8b6d69624e17fec3ddb
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-02-04 16:03:52 +0100

    Update 70

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,16 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from "../users/users.service";
+import { User } from "../users/users.entity";
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
 
-  async signIn(username: string, password: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
+  async signIn(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(email);
     if (user?.password !== password) {
         throw new UnauthorizedException('Invalid credentials');
     }
-
-    const { password: _, ...result } = user;
-    return result;
+    return { message: 'Sign in successful', userId: user.id , userName: user.name, userEmail: user.email };
   }
 }
