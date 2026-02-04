0d5195ce206c20a2ceba7f649b7b30d3fdef36c2
Ayeni-Olakunle <ayeniolakunle98@gmail.com>
2026-02-04 16:44:18 +0100

    Update 73

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,22 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from "../users/users.service";
-import { User } from "../users/users.entity";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+) {}
 
-  async signIn(email: string, password: string): Promise<any> {
+  async signIn(email: string, password: string): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(email);
     if (user?.password !== password) {
         throw new UnauthorizedException('Invalid credentials');
     }
-    return { message: 'Sign in successful', userId: user.id , userName: user.name, userEmail: user.email };
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,24 +1,24 @@
-import { Strategy } from "passport-jwt";
-import { PassportStrategy } from "@nestjs/passport";
-import { Injectable } from "@nestjs/common";
-import { ExtractJwt } from "passport-jwt";
-import { AuthService } from "../auth.service";
+// import { Strategy } from "passport-jwt";
+// import { PassportStrategy } from "@nestjs/passport";
+// import { Injectable } from "@nestjs/common";
+// import { ExtractJwt } from "passport-jwt";
+// import { AuthService } from "../auth.service";
 
-@Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
-    constructor(private authService: AuthService) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET || 'defaultSecretKey',
-        });
-    }
+// @Injectable()
+// export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+//     constructor(private authService: AuthService) {
+//         super({
+//             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//             ignoreExpiration: false,
+//             secretOrKey: process.env.JWT_SECRET || 'defaultSecretKey',
+//         });
+//     }
 
-    async validate(username: string, password: string): Promise<any> {
-        const user = await this.authService.validateUser(username, password);
-        if (!user) {
-            throw new Error('Unauthorized');
-        }
-        return user;
-    }
-}
+//     async validate(username: string, password: string): Promise<any> {
+//         const user = await this.authService.validateUser(username, password);
+//         if (!user) {
+//             throw new Error('Unauthorized');
+//         }
+//         return user;
+//     }
+// }
